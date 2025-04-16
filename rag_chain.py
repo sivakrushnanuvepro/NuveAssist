897da8c6a19bff007a1a25ea15c08c37cb94bf45
sivakrushna-works <sivakrushna.works@gmail.com>
2025-04-16 15:38:59 +0530

    new changeds added

--- rag_chain.py
+++ rag_chain.py
@@ -2,13 +2,17 @@ from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
+
+api_key = st.secrets["GOOGLE_API_KEY"]
+#api_key = os.getenv("GOOGLE_API_KEY")
 
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 def load_rag_chain():
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.getenv("GOOGLE_API_KEY"))
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=api_key)
     db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
 
     retriever = db.as_retriever()
